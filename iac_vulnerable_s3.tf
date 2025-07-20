7827097411b279f74273b0c8695bb47cac2622ee
eyalsagiwiz <eyal.sagi@wiz.io>
2025-07-20 14:10:25 +0300

    Update iac_vulnerable_s3.tf

--- iac_vulnerable_s3.tf
+++ iac_vulnerable_s3.tf
@@ -1,4 +1,4 @@
-# vulnerable_s3.tf
+# vulnerable_s3s.tf
 resource "aws_s3_bucket" "example_bucket" {
   bucket = "my-highly-vulnerable-unique-bucket-name-12345"
   acl    = "public-read" # This is the misconfiguration! Making an S3 bucket publicly readable is often a security risk.
